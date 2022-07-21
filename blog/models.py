de62b1414ca25d4372d55519cddea57db22529ab
Рязанов Валерий <domino-sender@mail.ru>
2022-07-21 11:06:37 +0000

    Finish custom filters

--- blog/models.py
+++ blog/models.py
@@ -13,7 +13,9 @@ from django.contrib.contenttypes.fields import GenericRelation
 
 class Tag(models.Model):
     value = models.TextField(max_length=100)
-
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+    created_at = models.DateTimeField(auto_now_add=True,  blank=True)
+    modified_at = models.DateTimeField(auto_now=True)
     def __str__(self):
         return self.value
 
@@ -28,14 +30,15 @@ class Comment(models.Model):
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
-    created_at = models.DateTimeField(auto_now_add=True)
-    modified_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True,  blank=True)
+    modified_at = models.DateTimeField(auto_now=True,  blank=True)
     published_at = models.DateTimeField(blank=True, null=True)
     title = models.TextField(max_length=100)
     slug = models.SlugField()
     summary = models.TextField(max_length=500)
     content = models.TextField()
     tags = models.ManyToManyField(Tag, related_name="posts")
+  
 # This is only possible if it’s a model under your control, so we can use it to make fetching comments for a Post simple, but we can’t edit the User model so easily.
 # Now fetching the Comments for a Post is simple. The comments attribute acts like a RelatedManager allowing you to query, filter(), add(), create() and remove() Comments to a Post. Let’s see a short example of how to use it.    
     comments = GenericRelation(Comment)
